c114d9f1549746ea456330acc1e39f7a1c6a0eeb
Thomas Martins <thomas.martins1710@gmail.com>
2024-04-13 10:49:41 +0200

    chore: v1 fix css and i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
-import react from "@astrojs/react";
 import astroI18next from "astro-i18next";
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://thomas-martins.dev",
   integrations: [
     UnoCSS({
       injectReset: true,
     }),
     astroI18next(),
-    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: "fr",
+        locales: {
+          en: "en-US",
+          fr: "fr-FR",
+        },
+      },
+    }),
   ],
 });

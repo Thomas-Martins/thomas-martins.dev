2391cd44814cf5ecc6b0c233d2f59ddd0fb64e65
Thomas Martins <thomas.martins1710@gmail.com>
2024-07-27 21:57:41 +0200

    disable dev tool bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   site: "https://thomas-martins.dev",
   integrations: [
     UnoCSS({
